f82d5fcf0a421d8836cf929769d7f0a2cf2849ba
Siddharth Roychoudhury <debu2in@gmail.com>
2023-05-09 21:49:52 +0200

    aded homework sql file

--- schema.sql
+++ schema.sql
@@ -1,8 +1,28 @@
 
-create table assistants (
-    id int SERIAL PRIMARY KEY,
-    name varchar(50),
-    email
-    address
-    teacher_id int
-)
+/**
+
+ Design a school schema:
+ -A school would have classes
+ -Classes have teachers (teaching staff) 
+ -Classes have students
+ -Each Class has a Monitor ( Monitor is among the students) 
+ -A school has a Principal
+ -School also has practical-labs
+ -School also has an amphi-theatre
+ -Amphi-Theatre has a seating capacity, projector, operator etc.
+ -School has a cleaning staff
+ -School has a administrative staff
+ -School has a management council or a board or mgmt staff
+ -Students pay fees
+ -Each fees paid has a associated receipt
+ -School pays out salary as well
+
+ Think of all the possible relations
+ like Primary Key from which table would be referenced as a FK in which table etc.
+
+ Once the schema is in place: we will then make a REST-API to fill the database with data
+
+
+**/
+
+
